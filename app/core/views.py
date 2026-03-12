d3ff9b291e162c5c38d30d7bdecf2649b67f15ab
devirambudhathoki369 <devirambudhathoki369@gmail.com>
2026-03-12 18:38:22 +0545

    views updated

--- app/core/views.py
+++ app/core/views.py
@@ -520,7 +520,7 @@ def send_for_correction(request):
         user_level = current_user.level
         try:
             document = request.FILES["filename"]
-            if document.size > 5297152:
+            if document.size > 1024:
                 return JsonResponse({"message":"Please select image size no more than 2 MB"},status=400)
 
         except:
